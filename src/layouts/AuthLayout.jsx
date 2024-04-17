a5aef495a97c8ec697297808d7855ffa130294b4
clasesucatmarlon <clasesucatmarlon@gmail.com>
2024-04-17 17:46:58 -0500

    feat(tickets-front): :sparkles: Generando títulos relacionados al pass dinámicos
    Generando títulos relacionados al pass dinámicos

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 const AuthLayout = () => {
+
+    const {pathname} = useLocation();
+    
+    // GENERAR TÍTULO DINÁMICO
+    let title ="";
+
+    switch (pathname) {
+        case "/auth/login":
+            title = "Iniciar sesión";
+            break;
+        case "/auth/register":
+            title = "Crear cuenta";
+            break;
+        case "/auth/forgot-password":
+            title = "Recuperar password";
+            break;
+    }
+
     return (
-        <div>
-        Auth Layout
-        <Outlet />
-    </div>
+        <div className="min-h-screen flex flex-col items-center justify-center">
+            <h1>{pathname.startsWith("/auth/change-password/") ? "Actualizar password" :  title}</h1>
+            <main>
+                <Outlet />
+            </main>
+        </div>
     )
 }
 
