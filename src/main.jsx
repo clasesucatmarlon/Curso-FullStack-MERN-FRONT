8b241f24cabf65165eb0565c9209ca46bc2f0a53
clasesucatmarlon <clasesucatmarlon@gmail.com>
2024-04-19 14:31:02 -0500

    feat(tickets-front): :lipstick: Agregado componente para trabajas con mensajes emergentes
    Agregado componente para trabajas con mensajes emergentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'sonner';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <Toaster />
     <App />
   </BrowserRouter>,
 )
