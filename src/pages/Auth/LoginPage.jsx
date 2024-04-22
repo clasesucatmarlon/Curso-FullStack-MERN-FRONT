91794771ab624d6d84c18f79414d9822dddfcee7
clasesucatmarlon <clasesucatmarlon@gmail.com>
2024-04-22 13:23:40 -0500

    feat(tickets-front): :sparkles: Creado formulario y funcionalidad para login
    Creado formulario y funcionalidad para login

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -1,7 +1,92 @@
+import { useState } from "react";
+import { Link, useNavigate, Navigate } from "react-router-dom";
+import { toast } from "sonner";
+import api from "../../lib/Axios";
+import Label from "../../components/commons/Label";
+import InputPassword from "../../components/commons/InputPassword";
+import Input from "../../components/commons/Input";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    if ([email, password].includes("")) {
+      return toast.error("Todos los campos son obligatorios");
+    }
+
+    try {
+      const { data } = await api.post("/users/login", {
+        email,
+        password,
+      });
+
+      if (data.response === "success") {
+        localStorage.setItem("token-tickets", data.token);
+        // setAuth(data.user);
+        console.log("DATA: ", data.user)
+        setEmail("");
+        setPassword("");
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(`[LOGIN_USER_ERROR]: ${error}`);
+      toast.error(error.response.data.message);
+    }
+  };
+
+  // if (auth._id) return <Navigate to="/" />;
+
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmit} className="space-y-7">
+      <div className="w-full max-w-lg relative">
+        <Label contentLabel="Correo electrónico" />
+        <Input idInput="inputEmail" contentPlaceholder="correo@correo.com" contentValue={email} funcChange={setEmail} />
+      </div>
+      <div className="w-full max-w-lg relative">
+        <Label contentLabel="Contraseña" />
+        <InputPassword idInput="inputPassword" contentPlaceholder="******" contentValue={password} funcChange={setPassword} showPassword={showPassword}/>
+
+        <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute right-1 top-1/2 -translate-y-1/2 w-10 h-10 rounded-full hover:bg-gray-200 transition-colors duration-300"
+        >
+          {showPassword ? (
+            <i className="fi fi-rr-unlock"></i>
+          ) : (
+            <i className="fi fi-rr-lock"></i>
+          )}
+        </button>
+      </div>
+
+      <div className="text-right">
+        <Link to="/auth/forgot-password" className="text-sm text-gray-500">
+          ¿Olvidaste tu contraseña?
+        </Link>
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="w-full bg-black text-white rounded-full py-3 px-5"
+        >
+          Ingresar
+        </button>
+      </div>
+      <div className="text-center">
+        <p>
+          ¿No tienes una cuenta?{" "}
+          <Link to="/auth/register" className="font-bold hover:underline">
+            Registrate
+          </Link>
+        </p>
+      </div>
+    </form>
   )
 }
 
