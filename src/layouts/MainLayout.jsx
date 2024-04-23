507b93bd493f34b0b2a55f916bf8d74e34fbe8d1
clasesucatmarlon <clasesucatmarlon@gmail.com>
2024-04-23 16:30:57 -0500

    feat(tickets-front): :sparkles: Agregado componentes al layout principal
    Agregado componentes al layout principal

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,32 +1,42 @@
+import { useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 import useAuth from "../hooks/useAuth";
+import Sidebar from "../components/widgets/Sidebar";
+import Header from "../components/widgets/Header";
 
 const MainLayout = () => {
-    const { auth, isLoading, logout } = useAuth();
+	const [showSidebar, setShowSidebar] = useState(false);
 
-    if (isLoading) {
-        return (
-            <p>Cargando</p>
-        )
-    }
+	const { auth, isLoading} = useAuth();
 
-    return (
-        <>
-            {
-                auth._id ? (
-                    <div>
-                        <button onClick={() => logout()}>Cerrar sesión</button>
-                        <h3>{auth.firstName} {auth.lastName}</h3>
-                        <Outlet />
-                    </div>
-                ) : (
-                    <Navigate to="/auth/login" />
-                )
-            }
+	if (isLoading) {
+		return (
+			<p>Cargando</p>
+		)
+	}
 
-        </>
-    )
+	return (
+		<>
+			{
+				auth._id ? (
+					<div className="min-h-screen flex">
+						<Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar}/>
+						<div className="flex-1 ">
+							<Header setShowSidebar={setShowSidebar}/>
+							<main className="bg-gray-100 h-[92vh] p-5 lg:p-8 overflow-y-auto overflow-x-hidden">
+								<Outlet />
+							</main>
+						</div>
+
+					</div>
+				) : (
+					<Navigate to="/auth/login" />
+				)
+			}
+
+		</>
+	)
 }
 
 export default MainLayout;
